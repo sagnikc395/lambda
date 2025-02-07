c1d90de51dae9d074a71de33cd637cd229f856e5
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-02-07 08:49:30 +0530

    simple test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { evaluate } from "./evaluator";
+import { parseLambdaTerm } from "./parser";
+
+const input = "(\\x.\\y.x y) (\\z.z)";
+const term = parseLambdaTerm(input);
+const result = evaluate(term);
+
+console.log(JSON.stringify(result, null, 2));
